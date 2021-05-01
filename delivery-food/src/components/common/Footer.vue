b78425e99f83fa98618933a35a81ec7090dc59ac
jdavid777 <jdavidgarcia28@gmail.com>
2021-05-01 16:28:31 -0500

    Estilos aplicados a navbar y footer

--- delivery-food/src/components/common/Footer.vue
+++ delivery-food/src/components/common/Footer.vue
@@ -1,13 +1,30 @@
 <template>
-  <footer class="bg-dark-x py-5 border-top">
+  <footer class="bg-dark-x py-5 border-top bg-footer footer">
     <div class="container">
       <div class="row">
         <div class="col-lg-6">
           <p class="mb-0">
-            <a href="#" class="font-weight-bold">Delivery Food</a>.
-            <small>Derechos reservados 2021</small>
+            <a href="#" class="font-weight-bold name-app">Delivery Food</a>.
+            <small class="font-white">Derechos reservados 2021</small>
           </p>
-          <div>
+        </div>
+        <div class="col-lg-6 text-right font-white">
+          <ul class="list-inline mb-0">
+            <li class="list-inline-item mr-3"><small>Síguenos en:</small></li>
+            <li class="list-inline-item mr-3">
+              <i class="icon ion-logo-instagram lead"></i>
+            </li>
+            <li class="list-inline-item mr-3">
+              <i class="icon ion-logo-linkedin lead"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="icon ion-logo-facebook lead"></i>
+            </li>
+          </ul>
+        </div>
+        <div class="col-lg-6 text-left">
+          <ul class="list-inline-item ">
+            <li class="list-inline-item">
               <a
                 class="nav-link"
                 href="https://getbootstrap.com/docs/4.6/getting-started/introduction/"
@@ -15,10 +32,13 @@
               >
                 Docs Bootstrap
               </a>
+            </li>
+            <li class="list-inline-item">
               <a class="nav-link" href="#">
                 <router-link to="/docs-vue">Docs Vue</router-link>
               </a>
-            </div>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
@@ -31,7 +51,14 @@ export default {
 };
 </script>
 <style scoped>
-a{
-  color: orangered !important;
+.name-app{
+  color:var(--primary-x)
+}
+.bg-footer {
+  background-color: var(--dark-xx);
+}
+.footer{ 
+  max-height: var(--height-footer);
+
 }
 </style>
