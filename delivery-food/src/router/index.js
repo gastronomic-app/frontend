bb3a7ec9ec9cfad5f2594d97161fc2d04b3491e3
jdavid777 <jdavidgarcia28@gmail.com>
2021-09-23 10:57:06 -0500

    solucion a bugs del componenete al momento de dar click en el icono de obtener la ubicacion del usuario

--- delivery-food/src/router/index.js
+++ delivery-food/src/router/index.js
@@ -35,11 +35,6 @@ const routes = [
     component: () => import("@/views/user/Edit.vue"),
   },
   {
-    path: "/test",
-    name: "test",
-    component: () => import("@/components/geolocation/test.vue"),
-  },
-  {
     path: "/register",
     name: "Register",
     component: () => import("@/views/user/Register.vue"),
